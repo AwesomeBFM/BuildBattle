56fdc54ce734f9606e151612837173622fb59ffd
AwesomeBFM <55819926+AwesomeBFM@users.noreply.github.com>
2022-10-25 08:55:21 -0600

    Initial Commit

---
+++ src/main/java/dev/awesomebfm/buildbattle/BuildBattle.java
@@ -0,0 +1,17 @@
+package dev.awesomebfm.buildbattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuildBattle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
